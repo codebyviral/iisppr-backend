efd799804683b6a0bad1e8f93ffc1a0f23368008
prathmesh_khandare <prathameshkhandare1551@gmail.com>
2024-12-20 09:55:28 +0530

    indexing issue resolved

--- Models/AttendanceModel.js
+++ Models/AttendanceModel.js
@@ -1,32 +1,32 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const AttendanceSchema = new mongoose.Schema({
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User', 
-        required: true,
-    },
-    date: {
-        type: Date,
-        required: true,
-    },
-    status: {
-        type: String,
-        enum: ['Present', 'Absent', 'Late'],
-        required: true,
-    },
-    CheckInTime: {
-        type: Date,
-        default: function () {
-            const currentDate = new Date();  // Get the current local time
-            return currentDate;  // Store the local time directly
-        }, // Automatically records when attendance is marked
-    },
-    CheckOutTime:{
-        type:Date,
-    }
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["Present", "Absent", "Late"],
+    required: true,
+  },
+  CheckInTime: {
+    type: Date,
+    default: function () {
+      const currentDate = new Date(); // Get the current local time
+      return currentDate; // Store the local time directly
+    }, // Automatically records when attendance is marked
+  },
+  CheckOutTime: {
+    type: Date,
+  },
 });
 
 // Ensure no duplicate attendance for the same intern on the same date
 AttendanceSchema.index({ userId: 1, date: 1 }, { unique: true });
-export default  mongoose.model('Attendance', AttendanceSchema);
+export default mongoose.model("Attendance", AttendanceSchema);
